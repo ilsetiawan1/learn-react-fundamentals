093d79ae5355bab31a448d929d055421d1391c4f
Muhammad Ilham Setiawan <m.ilsetiawan1@gmail.com>
2025-09-04 14:37:38 +0700

    Belajar mapping komponen dan state dengan useState

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,13 +1,32 @@
+import { useEffect, useState } from 'react';
 import Article from '../components/Article';
 import posts from '../posts.json';
+import TestOtak from '../components/TestOtak';
 
 function Homepage() {
+
+  const [search, setSearch] = useState('')
+
+  const changeSearch = (event) => {
+    // console.log(event.target.value)
+    setSearch(event.target.value)
+  }
+
   return (
-    <div className='w-full text-center mt-10'>
+    <div className="w-full text-center mt-10">
       <h1>Simple Blog</h1>
-      {posts.map((blog) => {
-        return <Article title={blog.title} tags={blog.tags} date={blog.date} />;
-      })}
+      <div className="">
+        Cari Artikel : <input onChange={changeSearch} type="text" className="border-2" />
+      </div>
+      <div>{search}</div>
+      {posts.map(({ title, tags, date }, index) => (
+        // <Article title={title} tags={tags} date={date} />
+        <Article key={index} title={title} tags={tags} date={date} />
+      ))}
+      <div className='bg-slate-200 w-full h-[400px]'>
+        <TestOtak/>
+
+      </div>
     </div>
   );
 }
